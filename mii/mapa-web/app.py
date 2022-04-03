a5c6a5de59feccfbccd4fe8ac49c7cc7d138af17
Gloria Macia <gloria.macia@roche.com>
2022-04-03 20:59:25 +0200

    add geteccu spanish hospitals

--- mii/mapa-web/app.py
+++ mii/mapa-web/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/geteccu")
+def geteccu():
+    return render_template('geteccu_unidades.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
